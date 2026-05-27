182964f980275cff04f8b1dd32e4f1dfcd4c9521
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-05-27 19:28:01 +0300

    Added settings classes to koin

--- composeApp/src/commonMain/kotlin/com/example/kpmapp_with_di/data/about/AboutRepository.kt
+++ composeApp/src/commonMain/kotlin/com/example/kpmapp_with_di/data/about/AboutRepository.kt
@@ -1,10 +1,17 @@
 package com.example.kpmapp_with_di.data.about
 
+import com.example.kpmapp_with_di.data.common.preferences.Preferences
+import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import kotlin.math.max
 import kotlin.math.min
+import kotlin.time.Clock
 
 internal class AboutRepository(
-    private val platform: Platform
+    private val platform: Platform,
+    private val preferences: Preferences
 ) {
     fun getAbout(): List<Pair<String, String>> {
         val items = mutableListOf(
@@ -24,5 +31,26 @@ internal class AboutRepository(
         items.add("Display" to display)
 
         return items
+    }
+
+    fun increaseVisitCount() {
+        preferences.aboutVisitedCount++
+    }
+
+    fun visitedCount(): Int {
+        return preferences.aboutVisitedCount
+    }
+
+    fun visitedCountObservable(): Flow<Int> {
+        return preferences.observableAboutVisitedCount
+    }
+
+    fun updateVisitedDate() {
+        preferences.aboutVisitedDate = Clock.System.now()
+            .toLocalDateTime(TimeZone.currentSystemDefault())
+    }
+
+    fun visitedDate(): LocalDateTime? {
+        return preferences.aboutVisitedDate
     }
 }
--- composeApp/src/commonMain/kotlin/com/example/kpmapp_with_di/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/com/example/kpmapp_with_di/di/AppModule.kt
@@ -2,10 +2,24 @@ package com.example.kpmapp_with_di.di
 
 import com.example.kpmapp_with_di.data.about.AboutRepository
 import com.example.kpmapp_with_di.data.about.Platform
+import com.example.kpmapp_with_di.data.common.preferences.AppPreferences
+import com.example.kpmapp_with_di.data.common.preferences.Preferences
 import com.example.kpmapp_with_di.ui.about.AboutViewModel
+import com.russhwolf.settings.ObservableSettings
+import com.russhwolf.settings.Settings
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
+import org.koin.dsl.binds
 import org.koin.dsl.module
 import org.koin.plugin.module.dsl.single
 import org.koin.plugin.module.dsl.viewModel
+
+private fun createSettings(): Settings = Settings()
+
+val dataModule = module {
+    single<Settings> { createSettings() } binds arrayOf(Settings::class, ObservableSettings::class)
+    singleOf(::AppPreferences) bind Preferences::class
+}
 
 val appModule = module {
     single<Platform>()
