ac9998e01b0e9f9c58afae606c83695f76682814
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-05-28 23:33:37 +0300

    Fix DI for each platform

--- composeApp/src/jvmMain/kotlin/com/example/kpmapp_with_di/main.kt
+++ composeApp/src/jvmMain/kotlin/com/example/kpmapp_with_di/main.kt
@@ -2,8 +2,10 @@ package com.example.kpmapp_with_di
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.example.kpmapp_with_di.di.initKoin
 
 fun main() = application {
+    initKoin()
     Window(
         onCloseRequest = ::exitApplication,
         title = "KPMapp_with_Di",
--- composeApp/src/webMain/kotlin/com/example/kpmapp_with_di/main.kt
+++ composeApp/src/webMain/kotlin/com/example/kpmapp_with_di/main.kt
@@ -2,9 +2,11 @@ package com.example.kpmapp_with_di
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import com.example.kpmapp_with_di.di.initKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    initKoin()
     ComposeViewport {
         App()
     }
